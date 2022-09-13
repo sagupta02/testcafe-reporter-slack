a319638a2384998319aabdf615d65908e0883094
SarthakGupta <sarthak.gupta@usabilla.com>
2022-09-13 14:10:51 +0200

    Add functions to push slack message

--- src/SlackMessage.js
+++ src/SlackMessage.js
@@ -1,12 +1,20 @@
-import config from './config';
+// import config from './config';
 import loggingLevels from "./const/LoggingLevels";
+import WebClient from "@slack/web-api";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const token = process.env.SLACK_TOKEN;
+const testTagArray = [];
+
+const conversationId = process.env.TESTCAFE_SLACK_CHANNEL;
 
 export default class SlackMessage {
   constructor() {
-    let slackNode = require('slack-node');
-    this.slack = new slackNode();
-    this.slack.setWebhook(config.webhookUrl);
-    this.loggingLevel = config.loggingLevel;
+    
+    this.slack = new WebClient(token);
+    
+    this.loggingLevel = 'TEST';
     this.messages = [];
     this.errorMessages = [];
   }
@@ -20,7 +28,73 @@ export default class SlackMessage {
   }
 
   sendMessage(message, slackProperties = null) {
-    this.slack.webhook(Object.assign({
+    this.slack.chat.postMessage({
+      channel: config.channel,
+      text: '',
+      blocks: [{
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": ":warning: Test cafe Report Staging",
+          "emoji": true
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": ">>>Test Tags: @smoke\n:no_entry: *1/10 failed*"
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "section",
+        "fields": [
+          {
+            "type": "mrkdwn",
+            "text": ">>>*Failed tests:*\n:no_entry: One dashboard, view all link leads to dashboards page - failed\n:no_entry: One dashboard, view all link leads to salesforce page - failed"
+          }
+        ]
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "emoji": true,
+              "text": "Gitlab Pipeline"
+            },
+            "url": "https://gitlab.usabilla.net/usabilla/dev/direct/test-cafe-shaker/-/jobs/2133149",
+            "value": "click_me_123"
+          },
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "emoji": true,
+              "text": "Test Report"
+            },
+            "url": "https://www.intranet.usabilla.nl/qa/testcafe/325287/2133149/",
+            "value": "click_me_123"
+          }
+        ]
+      }
+      ]
+    }, function (err, response) {
+      if (!config.quietMode) {
+        if (err) {
+          console.log('Unable to send a message to slack');
+          console.log(response);
+        } else {
+          console.log(`The following message is send to slack: \n ${message}`);
+        }
+      }
+    })
+/*     this.slack.webhook(Object.assign({
       channel: config.channel,
       username: config.username,
       text: message
@@ -33,7 +107,7 @@ export default class SlackMessage {
           console.log(`The following message is send to slack: \n ${message}`);
         }
       }
-    })
+    }) */
   }
 
   sendTestReport(nrFailedTests) {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import config from './config';
-import SlackMessage from './SlackMessage';
-import LoggingLevels from './const/LoggingLevels';
-import emojis from './utils/emojis';
-import { bold, italics } from './utils/textFormatters';
+// import config from './config';
+// import SlackMessage from './SlackMessage';
+// import LoggingLevels from './const/LoggingLevels';
+// import emojis from './utils/emojis';
+// import { bold, italics } from './utils/textFormatters';
 
-const { loggingLevel } = config;
+// const { loggingLevel } = config;
+import WebClient from "@slack/web-api";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const token = process.env.SLACK_TOKEN;
+const conversationId = process.env.TESTCAFE_SLACK_CHANNEL;
+console.log(token);
+
+const testTagArray = [];
+let messages = [];
+
+const web = new WebClient(token);
+
+const generateSection = (messages) => {
+  // eslint-disable-next-line prettier/prettier
+  const newMessage = messages.join("\n:no_entry: ");
+  console.log(`*Failed tests:*\n:no_entry: ${newMessage}`);
+  return {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `*Failed tests:*\n:no_entry: ${newMessage}`,
+    },
+  };
+};
 
 export default function () {
   return {
@@ -12,32 +37,47 @@ export default function () {
     noColors: true,
 
     reportTaskStart(startTime, userAgents, testCount) {
-      this.slack = new SlackMessage();
+      /* Gets executed before all actions */
       this.startTime = startTime;
       this.testCount = testCount;
 
-      const startTimeFormatted = this.moment(this.startTime).format('M/D/YYYY h:mm:ss a');
+      // const startTimeFormatted = this.moment(this.startTime).format('M/D/YYYY h:mm:ss a');
 
-      this.slack.sendMessage(`${emojis.rocket} ${'Starting TestCafe:'} ${bold(startTimeFormatted)}\n${emojis.computer} Running ${bold(testCount)} tests in: ${bold(userAgents)}\n`)
+      console.log(`Report task start is running now with ${userAgents}!!!!`);
     },
 
     reportFixtureStart(name, path) {
       this.currentFixtureName = name;
 
-      if (loggingLevel === LoggingLevels.TEST) this.slack.addMessage(bold(this.currentFixtureName));
+      console.log('Report fixture start is running now!!!!');
     },
 
-    reportTestDone(name, testRunInfo) {
-      const hasErr = !!testRunInfo.errs.length;
-      let message = null;
+    reportTestDone(name, testRunInfo, meta) {
+      // const hasErr = !!testRunInfo.errs.length;
+      this.testRunInfo = testRunInfo;
+      // Scan the array for the value of the tag, If it exists , skip it . If not add it
+      if (!testTagArray.includes(meta.testType)) {
+        testTagArray.push(meta.testType);
+      }
+
+       /* here we need a local variable to store the failed test name and then push to global array of message */
+
+       if (this.testRunInfo.errs.length > 0) {
+        this.failestestname = name;
+        messages.push(this.failestestname);
+        console.log(this.testRunInfo.errs.length);
+      } 
+
+      console.log('Report test done is running now!!!!');
+      /* let message = null;
 
       if (testRunInfo.skipped) {
         message = `${emojis.fastForward} ${italics(name)} - ${bold('skipped')}`;
       } else if (hasErr) {
         message = `${emojis.fire} ${italics(name)} - ${bold('failed')}`;
-        this.renderErrors(testRunInfo.errs);
+        this.renderErrors(testRunInfo.errs);// we dont need it this is for exception
       } else {
-        message = `${emojis.checkMark} ${italics(name)}`
+        message = `${emojis.checkMark} ${italics(name)}` // we dont need to show pass tests
       }
 
       if (loggingLevel === LoggingLevels.TEST) this.slack.addMessage(message);
@@ -46,11 +86,11 @@ export default function () {
     renderErrors(errors) {
       errors.forEach((error, id) => {
         this.slack.addErrorMessage(this.formatError(error, `${id + 1} `));
-      })
+      }) */
     },
 
     reportTaskDone(endTime, passed, warnings, result) {
-      const endTimeFormatted = this.moment(endTime).format('M/D/YYYY h:mm:ss a');
+/*       const endTimeFormatted = this.moment(endTime).format('M/D/YYYY h:mm:ss a');
       const durationMs = endTime - this.startTime;
       const durationFormatted = this.moment
         .duration(durationMs)
@@ -71,7 +111,78 @@ export default function () {
       const message = `\n\n${finishedStr} ${durationStr} ${summaryStr}`;
 
       this.slack.addMessage(message);
-      this.slack.sendTestReport(this.testCount - passed);
+      this.slack.sendTestReport(this.testCount - passed); */
+
+      this.testResult = result;
+
+      const blockBodyTestMessage = generateSection(messages);
+
+      const blockBodyMain = [
+        {
+          type: 'header',
+          text: {
+            type: 'plain_text',
+            text: ':warning: Test cafe Report Staging',
+            emoji: true,
+          },
+        },
+        {
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: `*Test Tags:*\n@${testTagArray.join(' @').toString()}`,
+            },
+            {
+              type: 'mrkdwn',
+              text: `*Result:*\n${this.testResult.failedCount}/${this.testCount} failed`,
+            },
+          ],
+        },
+        {
+          type: 'divider',
+        },
+      ];
+
+      const blockBodyAction = {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              emoji: true,
+              text: 'Gitlab Pipeline',
+            },
+            url: 'https://gitlab.usabilla.net/usabilla/dev/direct/test-cafe-shaker/-/jobs/2133149',
+            value: 'click_me_123',
+          },
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              emoji: true,
+              text: 'Test Report',
+            },
+            url: 'https://www.intranet.usabilla.nl/qa/testcafe/325287/2133149/',
+            value: 'click_me_123',
+          },
+        ],
+      };
+
+      blockBodyMain.push(blockBodyTestMessage);
+      blockBodyMain.push(blockBodyAction);
+      // Post a message to the channel, and await the result.
+      await web.chat.postMessage({
+        text: 'Hello world!',
+        channel: conversationId,
+        blocks: blockBodyMain,
+      });
+
+      // The result contains an identifier for the message, `ts`.
+      console.log(
+        `Successfully send message in conversation ${conversationId}`
+      );
     }
   }
 }
